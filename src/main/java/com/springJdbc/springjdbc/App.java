6486c5ac6578124f105e83211527c004eefd101d
Ankit0607 <ankit.mishra06072000@gmail.com>
2024-03-28 23:29:26 +0530

    inserted the data into db using the application context and query method

--- src/main/java/com/springJdbc/springjdbc/App.java
+++ src/main/java/com/springJdbc/springjdbc/App.java
@@ -1,13 +1,26 @@
 package com.springJdbc.springjdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hey It's started now ..... :) :)");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springJdbc/springjdbc/config.xml");
+		JdbcTemplate t1 = context.getBean("jdbcTemplate", JdbcTemplate.class);
+
+		// ------- insert query -----
+		String query = "insert into student(id, name, city) values (?,?,?)";
+		// --- fire query ---
+		int result = t1.update(query, 011, "Krishna", "Mathura");
+
+		System.out.println("Number of record inserted: " + result);
+
+	}
 }
